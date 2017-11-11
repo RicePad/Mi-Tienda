b382df2746ee16449d0f8362bbe867246a643fbe
JON <jonn.thinkrtc@gmail.com>
2017-11-10 21:37:43 -0800

    Setup RESTAPI Routes for gigs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   resources :gigs
-  get 'homes/index'
-
-
   root to: 'gigs#index';
+
+
+  #REST API Route:
+
+  namespace :api do 
+  	namespace :v1 do
+  		resources :gigs
+  	end
+  end  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
