cb430e8d342f4b346799fb42863a9ccd70bba8d8
JON <jonn.thinkrtc@gmail.com>
2017-11-12 18:20:59 -0800

    set up gig show action and view

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,4 +1,5 @@
 class GigsController < ApplicationController
+	before_action :set_gig, only: [:show]
 	layout 'gig'	
 
 	def index
@@ -6,6 +7,25 @@ class GigsController < ApplicationController
 	end
 	
 	def show
+
 	end
+
+	def new
+
+	end
+
+	def show
+
+	end
+
+	private
+
+		def set_gig
+			@gig_item = Gig.find(params[:id])
+		end
+
+		def gig_params
+			params.require(:gig).permit(:title, :category, :description, :price, :main_image, :thumb_image, :status)
+		end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'gigs#index'
   resources :gigs
-  root to: 'gigs#index';
-
 
   #REST API Route:
 
