a004a0b15852945c353c5d603109741e8eb2e6fe
JON <jonn.thinkrtc@gmail.com>
2017-11-12 17:01:20 -0800

    Integraed React State for Gig index

--- app/javascript/components/GigList.jsx
+++ app/javascript/components/GigList.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 
-class GigList extends Component{
+class GigList extends Component {
 	constructor(props){
 		super(props);
 
@@ -12,15 +12,37 @@ class GigList extends Component{
 	}
 
 
-	fetchGigs(){
+	componentDidMount(){
+		axios.get("http://localhost:5000/api/v1/gigs.json")
+			.then(response => {
+				console.log(response)
+				this.setState({ gigs: response.data})
+			})
 
-	}
-
+			.catch(error => console.log(error))
+	}			
+	
 	render(){
+			console.log('this.state', this.state);
+		
 		return(
-			<h1>GigList</h1>
+			<div>
+				<h1>Gig List</h1>
+				{
+					this.state.gigs.map((gig, index) => {
+						return(
+								<div>
+									<h2 key={index}>{gig.title}</h2>
+								</div>
+							)
+					})
+				}
+			</div>
 			)
 	}
 }
+
+
+
 
 export default GigList;
---
+++ app/javascript/packs/gig_react.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GigList from '../components/GigList'
+
+
+const nameComponent = document.getElementById('gig-list');
+
+ReactDOM.render(
+	<GigList />, nameComponent
+	);
+
