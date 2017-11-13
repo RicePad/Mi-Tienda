82298d1dd3ec0fce44f40150cc24074934311b1d
JON <jonn.thinkrtc@gmail.com>
2017-11-12 22:40:41 -0800

    redux store successfully integrated

--- app/javascript/actions/index.jsx
+++ app/javascript/actions/index.jsx
@@ -1,12 +1,25 @@
 export const FETCH_GIGS = "FETCH_GIGS";
+import axios from 'axios';
 
 
-export default function recieveGigs(gigs) {
+ function recieveGigs(gigs) {
 	const action = {
 		type: FETCH_GIGS,
 		gigs
 	}
-	return action
+	return action;
 }
 
 
+export function fetchGigs(){
+	return(dispatch) => {
+		return axios.get('http://localhost:5000/api/v1/gigs.json')
+		.then(response => {
+			console.log(response)
+
+			dispatch(recieveGigs(response.data))
+
+		})
+		.catch(error => console.log(error))
+
+	}}	
--- app/javascript/packs/gigdetail_react.jsx
+++ app/javascript/packs/gigdetail_react.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GigDetail from '../components/GigDetail';
+import { fetchGigs } from '../actions';
 
 // Redux Setup
 import { Provider } from 'react-redux';
@@ -8,9 +9,11 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
 
-const componentName = document.querySelector('.gig-detail')
-const store = createStore(reducers)
-console.log('store.getState()', store.getState())
+const componentName = document.querySelector('.gig-detail');
+const store = createStore(reducers, applyMiddleware(thunk));
+// console.log('store.getState()', store.getState())
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(fetchGigs())
 
 
 ReactDOM.render(
