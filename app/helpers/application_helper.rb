8842e8ab5d08a2971d80333b564589d0f3605bc9
JON <jonn.thinkrtc@gmail.com>
2017-11-14 17:14:02 -0800

    implemented user authentication nav and created helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
-end
+
+	def login_helper
+		if current_user.is_a?(User)
+			link_to "Log out", destroy_user_session_path, method: :delete
+		else
+			(link_to "Sign Up", new_user_registration_path) + "<br>".html_safe +
+			(link_to "Log In", new_user_session_path)
+			end
+		end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'registration'}
   root to: 'gigs#index'
   resources :gigs
+  get 'home', to: 'homes#index'
 
   #REST API Route:
 
