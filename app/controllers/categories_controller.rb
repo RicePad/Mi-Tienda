e2780724c61d92f53228ccf5dd273cdc942bab06
JON <jonn.thinkrtc@gmail.com>
2017-11-15 14:01:57 -0800

    add category show action view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,18 @@
 class CategoriesController < ApplicationController
+	before_action :set_category, only: [:show]
+
 	def index
 		@categories = Category.all
 	end
+
+	def show 
+	 	@gig_item = Gig.find(params[:id])
+
+	end
+
+	private
+
+	  def set_category
+	  	@category = Category.find(params[:id])
+	  end
 end
