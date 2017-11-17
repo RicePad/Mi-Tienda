3ea8a4638bb61f81ca41c51cb881b6d7e45ef9ce
JON <jonn.thinkrtc@gmail.com>
2017-11-17 11:40:10 -0800

    set up first_name and last_name virtual attributes for user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :gigs
+  validates_presence_of :name
 
   def set_defaults
 		self.profile_image ||= "http://via.placeholder.com/350x200"
 	end
 
+         
+         def first_name
+          self.name.split.first
+         end
+         
+         def last_name
+          self.name.split.last
+         end
 end
