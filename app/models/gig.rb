9b251d3f039031a2240a8d27a5d6dada4d1dfc63
JON <jonn.thinkrtc@gmail.com>
2017-11-17 13:45:43 -0800

    added mount_uploader helper on gig.rb model

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -2,7 +2,11 @@ class Gig < ApplicationRecord
 	belongs_to :user
 	has_many :gig_categories
 	has_many :categories, through: :gig_categories
+	validates_presence_of :thumb_image, :main_image
 
+
+	mount_uploader :thumb_image, GigUploader
+	mount_uploader :main_image, GigUploader
 	
 	def set_defaults
 		self.main_image ||= "http://via.placeholder.com/600x400"
--- app/uploaders/gig_uploader.rb
+++ app/uploaders/gig_uploader.rb
@@ -5,13 +5,17 @@ class GigUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :aws
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -36,9 +40,7 @@ class GigUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
