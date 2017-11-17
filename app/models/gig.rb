215015fe41545aa1c6462fd21d3d5a634dabfccf
JON <jonn.thinkrtc@gmail.com>
2017-11-17 13:35:44 -0800

    after installing carrierwave gem run rails g uploader Gig

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -3,6 +3,7 @@ class Gig < ApplicationRecord
 	has_many :gig_categories
 	has_many :categories, through: :gig_categories
 
+	
 	def set_defaults
 		self.main_image ||= "http://via.placeholder.com/600x400"
 		self.thumb_image ||= "http://via.placeholder.com/200x200"
