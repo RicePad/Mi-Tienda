1704f0fdf3fb4d8987714a34d7dc787bf7277445
JON <jonn.thinkrtc@gmail.com>
2017-11-17 11:24:58 -0800

    Created DeviseWhiteList Concern to add name to user registration

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,13 @@
+module DeviseWhitelist
+    extend ActiveSupport::Concern
+
+	 included do
+          before_action :add_other_params_to_devise, if: :devise_controller?
+    end
+
+	 def add_other_params_to_devise 
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      end
+        
+end
