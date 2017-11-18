d604c0ccb0e258272f844abdb4c99ffaaaf44345
JON <jonn.thinkrtc@gmail.com>
2017-11-17 20:44:37 -0800

    fix app banner component

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppBanner from '../components/AppBanner';
 
 
-const nameComponent = document.getElementById('App-banner');
 
 ReactDOM.render(
-	<AppBanner />, nameComponent
+
 	);
