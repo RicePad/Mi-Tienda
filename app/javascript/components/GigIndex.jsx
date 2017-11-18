ec371ca39a5578bf284744b2138eb098dd28d94b
JON <jonn.thinkrtc@gmail.com>
2017-11-17 21:09:13 -0800

    created GigIndex component

---
+++ app/javascript/components/GigIndex.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+class GigIndex extends Component {
+
+	render(){
+		return(
+			<div>GigInde Component</div>
+			)
+	}
+}
+
+export default GigIndex;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import GigIndex from '../components/GigIndex';
+
+// Redux Setup
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from '../reducers'; 
+import thunk  from 'redux-thunk';
 
 
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
+<Provider store={store}>
+	<BrowserRouter>
+		<Switch>
+			<Route path='/' component={GigIndex} />
+		</Switch>
+	</BrowserRouter>
+ </Provider>,  document.querySelector('.application')
 
 	);
