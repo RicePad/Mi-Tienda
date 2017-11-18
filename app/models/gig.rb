445311d99360fac7b4a527b12f835467a7b95f6f
JON <jonn.thinkrtc@gmail.com>
2017-11-17 19:53:41 -0800

    updated main and thumb image file upload with validation

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -2,16 +2,17 @@ class Gig < ApplicationRecord
 	belongs_to :user
 	has_many :gig_categories
 	has_many :categories, through: :gig_categories
+	# after_initialize :set_defaults
 	validates_presence_of :thumb_image, :main_image
 
 
 	mount_uploader :thumb_image, GigUploader
 	mount_uploader :main_image, GigUploader
 	
-	def set_defaults
-		self.main_image ||= "http://via.placeholder.com/600x400"
-		self.thumb_image ||= "http://via.placeholder.com/200x200"
-	end
+	# def set_defaults
+	# 	self.main_image ||= "http://via.placeholder.com/600x400"
+	# 	self.thumb_image ||= "http://via.placeholder.com/200x200"
+	# end
 
 	
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 1.times do | user |
-	user = User.create(email: 'admin@example.com', password: '123456', password_confirmation: '123456')
+	user = User.create(name: 'Krystal', email: 'admin@example.com', password: '123456', password_confirmation: '123456')
 	user.save
 end
 puts '1 user has beeen created'
 
 
-5.times do |gig|
-	@gig = Gig.create!(title: "SEO Expert", description: "SEO expert to help you rank first", price: 20, main_image: "http://via.placeholder.com/600x400", thumb_image: "http://via.placeholder.com/200x200", status: 0, user_id: "1")
-end
+# # # 5.times do |gig|
+# # # 	@gig = Gig.create!(title: "SEO Expert", description: "SEO expert to help you rank first", price: 20, main_image: "http://via.placeholder.com/600x400", thumb_image: "http://via.placeholder.com/200x200", status: 0, user_id: "1")
+# # # end
 
-puts '5 gigs have been created!'
+# # puts '5 gigs have been created!'
 
 
 3.times do |category|
