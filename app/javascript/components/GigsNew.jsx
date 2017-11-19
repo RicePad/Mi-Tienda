828465c093e140be3f26d0edcefaf3d09af477b2
JONATHAN <jonn.thinkrtc@gmail.com>
2017-11-19 13:56:19 -0800

    Added redux-form field components to GigsNew

--- app/javascript/components/GigsNew.jsx
+++ app/javascript/components/GigsNew.jsx
@@ -4,9 +4,53 @@ import { Field, reduxForm } from 'redux-form';
 
 
 class GigsNew extends Component{
+	
+
+	renderField(field){
+		return(	
+			<div className="form-group">
+			<label>{field.label}</label>
+				<input
+					type="text"
+					className="form-control"
+					{...field.input}
+				/>	
+			</div>
+
+			)
+	}
+
+
+
 	render(){
 		return(
-			<div>GigNew Component</div>
+			<form>
+				<Field
+					label="Title of Gig"
+					name="title"
+					component={this.renderField}
+				 />
+				 <Field
+				 	label="Categories"
+					name="categories"
+					component={this.renderField}
+				 />
+				 <Field
+				 	label="Description"
+					name="description"
+					component={this.renderField}
+				 />
+				 <Field
+				 	label="Main Image"
+				 	name="image"
+				 	component={this.renderField}
+				 />
+				 <Field
+				 	label="Thumb Image"
+				 	name="thumbimage"
+				 	component={this.renderField}
+				 />	
+			</form>
 			)
 	}
 }
