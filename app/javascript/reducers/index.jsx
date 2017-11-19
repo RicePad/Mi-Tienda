a47012500b902e0abcc810de98eb3dae24be8583
JONATHAN <jonn.thinkrtc@gmail.com>
2017-11-19 13:15:15 -0800

    import { reducer as form Reducer} in reducers/index.js

--- app/javascript/reducers/index.jsx
+++ app/javascript/reducers/index.jsx
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import grabGig from './fetch_gigs_reducer.jsx';
 import GigIndexReducer from './gig_index_reducer.jsx';
+import { reducer as formReducer } from 'redux-form';
 
 
 export default combineReducers({
 	grabGig,
-	gigs: GigIndexReducer
+	gigs: GigIndexReducer,
+	form: formReducer
 
 })
