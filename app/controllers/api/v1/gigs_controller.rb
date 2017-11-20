1c17c6587857f71364e0520b037db8e91d20418e
JON <jonn.thinkrtc@gmail.com>
2017-11-20 11:01:54 -0800

    setup action creators for post request and integrated connet logic into reduxform to pass this.props.createGig  into GigNew component

--- app/controllers/api/v1/gigs_controller.rb
+++ app/controllers/api/v1/gigs_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::GigsController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
+
 	skip_before_action :verify_authenticity_token
 
 		def index
@@ -6,15 +8,13 @@ class Api::V1::GigsController < ApplicationController
 			render json: @gig_items
 		end
 
-		def new
-		end
 		
 		def create
 			@gig_item = Gig.create(gig_params)
 			render json: @gig_item
 		end
 			
-		end
+		
 
 		def show
 			@gig_item = Gig.find(params[:id])
@@ -24,7 +24,7 @@ class Api::V1::GigsController < ApplicationController
 		private
 
 		def gig_params
-		   params.require(:gig).permit(:title, :category, :description, :price, :main_image, :thumb_image, :status, category_ids: [])
+			 params.require(:gig).permit(:title, :category, :description, :price, :main_image, :thumb_image, :status, category_ids: [])
 		end
 
 end
