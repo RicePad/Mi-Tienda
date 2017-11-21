2e98d7aeee320d26afaf73e032a35efc420ba4ca
JON <jonn.thinkrtc@gmail.com>
2017-11-21 11:46:26 -0800

    integrated gravatar for user profile image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	def login_helper
 		if current_user.is_a?(User)
-			link_to "Log out", destroy_user_session_path, method: :delete, class: "btn btn-danger"
+			# (link_to "Log out", destroy_user_session_path, method: :delete, class: "btn btn-danger")
 		else
 			(link_to "Sign Up", new_user_registration_path, class: "btn btn-success") +
 
@@ -10,4 +10,14 @@ module ApplicationHelper
 				)
 		end
 	end
+
+	def avatar_url(user)
+   		 gravatar = Digest::MD5::hexdigest(user.email).downcase
+	  if user.profile_image
+	     user.profile_image
+	  else
+		"https://www.gravatar.com/avatar/#{gravatar}.jpg?d=identicon&s=150"
+  end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117194420) do
+ActiveRecord::Schema.define(version: 20171121185702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20171117194420) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.text "about"
+    t.string "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
