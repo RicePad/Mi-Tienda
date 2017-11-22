968419e74901743690b33c56455a2e801365bde1
JON <jonn.thinkrtc@gmail.com>
2017-11-22 09:43:01 -0800

    integrated delete action with redux on GigShow and GigShowPage components

--- app/javascript/reducers/gig_index_reducer.jsx
+++ app/javascript/reducers/gig_index_reducer.jsx
@@ -5,7 +5,7 @@ import _ from 'lodash';
 export default function(state = {},  action) {
 	switch(action.type) {
 		case DELETE_GIG:
-			return _.omit(state, action.payload.data);
+			return _.omit(state, action.payload);
 		case FETCH_GIG:
 		    // ES5 syntax, add id to gig to create gig.id into the new ...object
 			    // const gig = action.payload.data;
