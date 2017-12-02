fa3e86b873197039ee9b9f30f312bf63e582163e
JON <jonn.thinkrtc@gmail.com>
2017-12-01 19:46:28 -0800

    added logic to message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,16 +4,19 @@ class MessagesController < ApplicationController
 
 
 	def create
-
+		message = @chatroom.messages.new(message_params)
+		message.user = current_user
+		message.save
+		redirect_to @chatroom	
 	end
 
 	private
 
-	def set_chatroom
+		def set_chatroom
+			@chatroom = Chatroom.find(params[:chatroom_id])
+		end
 
-	end
-
-	def message_params
-
-	end
+		def message_params
+			params.require(:message).permit(:body)
+		end
 end
