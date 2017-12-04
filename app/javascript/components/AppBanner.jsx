784fb6ec87c09b2ef85c2442a8577320ac2e6b02
JON <jonn.thinkrtc@gmail.com>
2017-12-04 11:15:22 -0800

    updated banner component

--- app/javascript/components/AppBanner.jsx
+++ app/javascript/components/AppBanner.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import bannerImg from '../assets/banner.gif';
+import bannerImg from '../assets/banner2.jpg';
 
 
 class AppBanner extends Component {
@@ -7,16 +7,28 @@ class AppBanner extends Component {
 
 	render(){
 		return(
-			<div>
-				<div className="row">
-				    <div className="col-md-12">
-				    	<img  src={ bannerImg } className="banner" />
-					</div>
-				</div>
+			 <div className="jumbotron jumbotron-billboard">
+				  <div className="img">
+					  	<img  src={ bannerImg } className="banner" />
+					  </div>
+					    <div className="container">
+					        <div className="row">
+					            <div className="col-md-12">
+					              <h2>A place where work just happens.</h2>
+					                <p>
+					                	Freelance services for the lean entrepreneurs.
+					                </p>
+					            </div>
+					        </div>
+					    </div>
 			</div>
-			)
+						)
 	}
 }
 
 
 export default AppBanner;
+
+
+
+
