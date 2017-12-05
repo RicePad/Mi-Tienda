7f25b7caae12ee9e6e204730c124e0bb4cef7ae2
JON <jonn.thinkrtc@gmail.com>
2017-12-05 10:38:55 -0800

    fix image width on show page

--- app/uploaders/gig_uploader.rb
+++ app/uploaders/gig_uploader.rb
@@ -3,7 +3,7 @@ class GigUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
+  
   # Choose what kind of storage to use for this uploader:
   storage :aws
   # storage :fog
@@ -14,6 +14,7 @@ class GigUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
