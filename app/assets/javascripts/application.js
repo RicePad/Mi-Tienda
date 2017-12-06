9928443bb1162c4775ca7d8cd505c547e7a79b38
JON <jonn.thinkrtc@gmail.com>
2017-12-06 11:11:45 -0800

    change position of application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require turbolinks
 //= require jquery3
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
